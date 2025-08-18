e1e3237b10be9466fa95e6e9a3ba1ada6845b584
Marcio Bueno <marciobueno@gmail.com>
2025-08-18 23:07:33 +0000

    alteração na mensagem do GET

--- ex01-express/api/index.js
+++ ex01-express/api/index.js
@@ -8,7 +8,7 @@ console.log("PYTHON_ROOT", process.env.PYTHON_ROOT);
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Bem-vindo ao Express de Márcio");
 });
 
 app.listen(3000, () => {
