8d550cc36dc56ec869951d68c92b7827b5b1113a
Marcio Bueno <marciobueno@gmail.com>
2025-09-01 23:02:54 +0000

    adicionando CRUD para usuários e mensagens (incompleto)

--- ex01-express/api/index.js
+++ ex01-express/api/index.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 console.log("Olá, Turma!");
 console.log("MY_SECRET", process.env.MY_SECRET);
@@ -20,6 +21,17 @@ app.use((req, res, next) => {
   next();
 });
 
+// Código para conseguir extrair o conteúdo do body da mensagem HTTP
+// e armazenar na propriedade req.body (utiliza o body-parser)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Código para registrar o usuário que está logado
+app.use((req, res, next) => {
+  req.me = users[1];
+  next();
+});
+
 app.get("/", (req, res) => {
   return res.send("Received a GET HTTP method");
 });
@@ -35,7 +47,80 @@ app.put("/", (req, res) => {
 app.delete("/", (req, res) => {
   return res.send("Received a DELETE HTTP method");
 });
+//=================== DATA ===================
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
+//=================== USERS ===================
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users));
+});
+
+app.get("/users/:userId", (req, res) => {
+  return res.send(users[req.params.userId]);
+});
+
+app.post("/users", (req, res) => {
+  return res.send("POST HTTP method on user resource");
+});
+
+app.put("/users/:userId", (req, res) => {
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
+});
+
+app.delete("/users/:userId", (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
+});
+
+//=================== MESSAGES (begin) ===================
+
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+app.post("/messages", (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text,
+    userId: req.me.id,
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
+});
+
+//=================== MESSAGES (end) ===================
+
+const port = process.env.PORT ?? 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
 });
