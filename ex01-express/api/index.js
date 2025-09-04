e7c00b6277b7e108a6aca4de0d5e0c10fe175c83
Marcio Bueno <marciobueno@gmail.com>
2025-09-04 22:40:35 +0000

    modularização do código (model e rotas em outros arquivos)
    injeção do context para utilizá-los nas rotas sem precisar importar

--- ex01-express/api/index.js
+++ ex01-express/api/index.js
@@ -1,11 +1,9 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
 
-console.log("Olá, Turma!");
-console.log("MY_SECRET", process.env.MY_SECRET);
-console.log("PYTHON_ROOT", process.env.PYTHON_ROOT);
+import models from "./models";
+import routes from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -26,98 +24,19 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Código para registrar o usuário que está logado
+// Código para injetar no context o usuário que está logado e os models
 app.use((req, res, next) => {
-  req.me = users[1];
+  req.context = {
+    models,
+    me: models.users[1],
+  };
   next();
 });
 
-app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method");
-});
-
-app.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
-});
-
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
-
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
-});
-//=================== DATA ===================
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch",
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids",
-  },
-};
-
-let messages = {
-  1: {
-    id: "1",
-    text: "Hello World",
-    userId: "1",
-  },
-  2: {
-    id: "2",
-    text: "By World",
-    userId: "2",
-  },
-};
-
-//=================== USERS ===================
-
-app.get("/users", (req, res) => {
-  return res.send(Object.values(users));
-});
-
-app.get("/users/:userId", (req, res) => {
-  return res.send(users[req.params.userId]);
-});
-
-app.post("/users", (req, res) => {
-  return res.send("POST HTTP method on user resource");
-});
-
-app.put("/users/:userId", (req, res) => {
-  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-});
-
-app.delete("/users/:userId", (req, res) => {
-  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-});
-
-//=================== MESSAGES (begin) ===================
-
-app.get("/messages", (req, res) => {
-  return res.send(Object.values(messages));
-});
-
-app.get("/messages/:messageId", (req, res) => {
-  return res.send(messages[req.params.messageId]);
-});
-
-app.post("/messages", (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.me.id,
-  };
-
-  messages[id] = message;
-
-  return res.send(message);
-});
-
-//=================== MESSAGES (end) ===================
+app.use("/", routes.root);
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 const port = process.env.PORT ?? 3000;
 
---
+++ ex01-express/api/routes/index.js
@@ -0,0 +1,11 @@
+import session from "./session";
+import user from "./user";
+import message from "./message";
+import root from "./root";
+
+export default {
+  root,
+  session,
+  user,
+  message,
+};
