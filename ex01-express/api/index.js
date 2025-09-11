67740587e65feb78de229e96735e48c7cc4663b4
Marcio Bueno <marciobueno@gmail.com>
2025-09-11 22:31:49 +0000

    corrigido o código da rota /session

--- ex01-express/api/index.js
+++ ex01-express/api/index.js
@@ -25,10 +25,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Código para injetar no context o usuário que está logado e os models
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: models.users[1],
+    me: await models.User.findByPk(1),
   };
   next();
 });
--- ex01-express/api/routes/session.js
+++ ex01-express/api/routes/session.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
+  return res.send(req.context.me);
 });
 
 export default router;
